6d524894e465db7a51931441a32c348c3eec8df2
Samuel Trew <samuel.trew@outlook.com>
2024-01-06 23:17:36 +0000

    Added mongoose and basic api that's broken

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ await import("./src/env.js")
 /** @type {import("next").NextConfig} */
 const config = {
    reactStrictMode: true,
+   experimental: {
+      esmExternals: "loose",
+      serverComponentsExternalPackages: ["mongoose"],
+   },
 }
 
 export default config
